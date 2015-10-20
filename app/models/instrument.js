4375145461ec59ec981cddb771150da5bdb5ce16
mapineda Pineda <mapineda1222@gmail.com>
2015-10-20 10:13:23 -0500

    created new controllers, models

--- app/models/instrument.js
+++ app/models/instrument.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var instrumentSchema = new Schema({
-	// model: String,
-	//type: String
+	model: String,
+	type: String
 });
 
 module.exports = mongoose.model('Instrument', instrumentSchema);
