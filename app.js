9454f6f126e63e1bc3be09e49d9fd8841e91ac4f
Jordan Johns <jordanrossjohns@gmail.com>
2015-10-22 13:49:07 -0500

    idk what im doing

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('express-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var instruments = require('./routes/instruments');
+
+var app = express();
+app.use(favicon(__dirname+'/public/favicon.ico'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(express.favicon(__dirname+"/public/favicon.ico"));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/instruments', instruments);
+
+// app get
+// app.get( '/destroy/:id', routes.destroy );
+
+// passport config
+var Account = require('./app/models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose connection for database (replace with MONGO_CONNECT once working)
+// mongoose.connect("mongodb://jortron:qazwsxedc@ds041144.mongolab.com:41144/gearheads"); //connect to our database
+mongoose.connect(process.env.MONGO_DB_CONN_GEARHEAD);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
